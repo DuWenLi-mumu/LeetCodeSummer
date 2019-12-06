d44186dce655fa42fc8733aca3ae4480a64947bb
杜文丽 <1874980400@qq.com>
2019-12-06 20:58:55 +0800

    超时了

--- src/greedy/RemoveDuplicateLetters316.java
+++ src/greedy/RemoveDuplicateLetters316.java
@@ -8,23 +8,21 @@ import java.util.*;
 public class RemoveDuplicateLetters316 {
     Set<Character> chars = new HashSet<>();
     Set<String> res = new HashSet<>();
-    String s = "";
-    public void subDeal(int i, Set<Character> tmpChars, String currStr) {
+    public void subDeal(char c, Set<Character> tmpChars, String currStr,String s) {
         if (tmpChars.isEmpty()) {
             res.add(currStr);
             return;
         }
-        if (tmpChars.contains(s.charAt(i))) {
-            currStr += s.charAt(i);
-            tmpChars.remove(s.charAt(i));
-            for (int j = i + 1; j < s.length(); j++) {
-                if (tmpChars.isEmpty()){
-                    res.add(currStr);
-                    return;
-                }
-                if (tmpChars.contains(s.charAt(j))) {
-                    subDeal(j, new HashSet<>(tmpChars), currStr);
-                }
+        if (tmpChars.contains(c)) {
+            currStr += c;
+            s=s.replace(c+"","");
+            tmpChars.remove(c);
+            if (tmpChars.isEmpty()){
+                res.add(currStr);
+                return;
+            }
+            for (int i = 0; i <s.length() ; i++) {
+                subDeal(s.charAt(i), new HashSet<>(tmpChars), currStr,s.substring(i));
             }
         }
 
@@ -34,14 +32,13 @@ public class RemoveDuplicateLetters316 {
         if (s.length() <= 1) {
             return s;
         }
-        this.s=s;
         for (int i = 0; i < s.length(); i++) {
             chars.add(s.charAt(i));
         }
         for (int i = 0; i < s.length(); i++) {
             Set<Character> tmpChars = new HashSet<>(chars);
             String currStr = "";
-            subDeal(i, tmpChars, currStr);
+            subDeal(s.charAt(i), tmpChars, currStr,s.substring(i));
         }
         List<String> resList=new ArrayList<>();
         for (String str:res
@@ -55,5 +52,6 @@ public class RemoveDuplicateLetters316 {
     public static void main(String[] args) {
         RemoveDuplicateLetters316 test = new RemoveDuplicateLetters316();
         System.out.println(test.removeDuplicateLetters("bbcaac"));
+        //System.out.println("bbbcc".substring(1));
     }
 }
