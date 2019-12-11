2e0bde572a5e7621c18d749f705adc6f40e6646f
杜文丽 <1874980400@qq.com>
2019-12-11 14:47:05 +0800

    tree

---
+++ src/tree/TreeNode.java
@@ -0,0 +1,13 @@
+package tree;
+
+/**
+ * Created by 杜文丽 on 2019/12/11 10:30
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
