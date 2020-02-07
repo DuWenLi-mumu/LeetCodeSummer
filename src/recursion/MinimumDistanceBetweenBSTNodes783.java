272661c58c498d23b9192d3f6cd474093c48cd04
杜文丽 <1874980400@qq.com>
2020-02-07 21:01:58 +0800

    BST树的中序遍历

---
+++ src/recursion/MinimumDistanceBetweenBSTNodes783.java
@@ -0,0 +1,29 @@
+package recursion;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 杜文丽 on 2020/2/7 20:47
+ **/
+public class MinimumDistanceBetweenBSTNodes783 {
+    int min=100000000;
+    ArrayList<Integer> arrayList=new ArrayList<>();
+    public int minDiffInBST(TreeNode root) {
+        if (root==null){
+            return 0;
+        }
+        dfs(root);
+        for (int i = 0; i <arrayList.size()-1 ; i++) {
+            min=Math.min(arrayList.get(i+1)-arrayList.get(i),min);
+        }
+        return min;
+    }
+    public void dfs(TreeNode root){
+        if (root==null){
+            return;
+        }
+        dfs(root.left);
+        arrayList.add(root.val);
+        dfs(root.right);
+    }
+}
