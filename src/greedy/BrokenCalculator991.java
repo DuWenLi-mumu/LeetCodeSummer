4d99916f0952f37021310eed4e7b500ab1264a42
杜文丽 <1874980400@qq.com>
2020-02-22 23:30:23 +0800

    坏掉的计算器

---
+++ src/greedy/BrokenCalculator991.java
@@ -0,0 +1,26 @@
+package greedy;
+
+/**
+ * Created by 杜文丽 on 2/22/2020 10:22 PM
+ * X只能*2或者-1
+ * 等价于
+ * y只能/2或者+1
+ **/
+public class BrokenCalculator991 {
+    public int brokenCalc(int X, int Y) {
+        if (X>=Y){
+            return X-Y;
+        }
+        int res=0;
+        while (X<Y){
+            if (Y%2==0){
+                res++;
+                Y/=2;
+            }else {
+                res+=2;
+                Y=(Y+1)/2;
+            }
+        }
+        return res+X-Y;
+    }
+}
