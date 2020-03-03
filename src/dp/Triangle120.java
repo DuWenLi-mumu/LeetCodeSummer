1379de5ea6d7aeabe24a67207d7369de8c1fdb91
杜文丽 <1874980400@qq.com>
2020-03-03 08:19:38 +0800

    DP

--- src/dp/Triangle120.java
+++ src/dp/Triangle120.java
@@ -6,24 +6,21 @@ import java.util.List;
  * Created by 杜文丽 on 2/29/2020 7:40 PM
  **/
 public class Triangle120 {
+    
     public int minimumTotal(List<List<Integer>> triangle) {
-        if (triangle.size()<=0){
-            return 0;
-        }else if (triangle.size()<=1){
-            int min=triangle.get(0).get(0);
-            for (int i = 1; i <triangle.get(0).size() ; i++) {
-                min=Math.min(min,triangle.get(0).get(i));
-            }
-            return min;
-        }
         int m=triangle.size();
+        if (m<=0){
+            return 0;
+        }
         int[][] dp=new int[m][m];
-        for (int i = m/2; i <m ; i++) {
-            for (int j = m/2; j <m ; j++) {
-                dp[i][j]=111111111;
+        for (int i = 0; i <m ; i++) {
+            dp[m-1][i]=triangle.get(m-1).get(i);
+        }
+        for (int i = m-2; i >=0 ; i--) {
+            for (int j = 0; j <triangle.get(i).size() ; j++) {
+                dp[i][j]=Math.min(dp[i+1][j+1]+triangle.get(i).get(j),dp[i+1][j]+triangle.get(i).get(j));
             }
         }
-
-
+        return dp[0][0];
     }
 }
