fa6148a851a848f03c4b7773e08ce5515da2a095
杜文丽 <1874980400@qq.com>
2020-03-29 17:04:23 +0800

    修改格式

--- src/dfs/MaximumDepthOfNaryTree559.java
+++ src/dfs/MaximumDepthOfNaryTree559.java
@@ -1,10 +1,34 @@
 package dfs;
 
+import util.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 杜文丽 on 3/29/2020 4:35 PM
  **/
 public class MaximumDepthOfNaryTree559 {
+    int dep=1;
     public int maxDepth(Node root) {
-
+        if (root==null){
+            return 0;
+        }
+        return sub(root.children);
+    }
+    public int sub(List<Node> root){
+        if (root.isEmpty()){
+            return dep;
+        }
+        dep++;
+        List<Node> children=new ArrayList<>();
+        for (Node kid: root
+             ) {
+            for (Node node:kid.children
+                 ) {
+                children.add(node);
+            }
+        }
+        return sub(children);
     }
 }
--- src/util/Node.java
+++ src/util/Node.java
@@ -1,11 +1,12 @@
 package util;
 
+
 import java.util.List;
 
 /**
  * Created by 杜文丽 on 3/29/2020 4:36 PM
  **/
-class Node {
+public class Node {
     public int val;
     public List<Node> children;
 
