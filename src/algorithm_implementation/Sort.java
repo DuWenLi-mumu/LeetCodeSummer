bc4c0e56818c6c647110d803b1fcb859d0552f97
杜文丽 <1874980400@qq.com>
2020-03-31 17:04:54 +0800

    快排

---
+++ src/algorithm_implementation/Sort.java
@@ -0,0 +1,38 @@
+package algorithm_implementation;
+
+/**
+ * Created by 杜文丽 on 3/31/2020 5:02 PM
+ **/
+public class Sort {
+    public int[] sortArray(int[] nums) {
+        if (nums.length<=1){
+            return nums;
+        }
+        quickSort(nums,0,nums.length-1);
+        return nums;
+    }
+    /*
+    快速排序
+     */
+    public void quickSort(int[] nums,int low,int high){
+        if (low>=high){
+            return;
+        }
+        int index=partition(nums,low,high);
+        quickSort(nums,low,index-1);
+        quickSort(nums,index+1,high);
+    }
+    public int partition(int[] nums,int l,int h){
+        int pivot=nums[l];
+        while (l<h){
+            while (l<h&&nums[h]>=pivot) h--;//右边找到一个小的
+            nums[l]=nums[h];
+
+            while (l<h&&nums[l]<=pivot) l++;//左边找到一个大的
+            nums[h]=nums[l];
+        }
+        nums[l]=pivot;
+        return l;
+    }
+
+}
