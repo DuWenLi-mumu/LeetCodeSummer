56741a3e33b5684d4cb7d6fa4c370f0dad6c692d
杜文丽 <1874980400@qq.com>
2020-09-17 22:43:31 +0800

    dp

--- src/dp/CoinChange322.java
+++ src/dp/CoinChange322.java
@@ -1,9 +1,6 @@
 package dp;
 
-import algorithm_implementation.Sort;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -48,22 +45,13 @@ public class CoinChange322 {
         return results.get(0);
     }
 
-    int[] dp;//计算到达amount所有可能的结果
-    int[] dp_mem;//记录计算过后的最小值
-    /**
-     * 动态规划实现 完全背包
-     * 遍历出所有可能的结果 取最小的结果
-     *
-     * @param coins
-     * @param amount
-     */
     public int solve2(int[] coins, int amount) {
-        if (amount==0){
+        if (amount == 0) {
             return 0;
-        }else if (coins.length==0&&amount!=0) {
+        } else if (coins.length == 0 && amount != 0) {
             return -1;
         }
-        return sub(coins,amount,new int[amount]);
+        return sub(coins, amount, new int[amount]);
     }
 
     public int sub(int[] coins, int amount, int[] count) {
@@ -88,14 +76,76 @@ public class CoinChange322 {
         return count[amount - 1];
     }
 
+    int[][] dp;//dp[i][j]表示钱i个货币凑成金额为j的最小数目
+    int max=11111111;
+    public int solve3(int[] coins, int amount) {
+        dp = new int[coins.length+1][amount + 1];
+        for (int i = 0; i <=coins.length; i++) {
+            for (int j = 0; j <=amount ; j++) {
+                dp[i][j]=max;
+            }
+        }
+        for (int i = 0; i <=coins.length ; i++) {
+            dp[i][0]=0;
+        }
+        for (int i = 1; i <= coins.length; i++) {
+        for (int j = 1; j <= amount; j++) {
+                if (j - coins[i-1] >= 0)
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i-1]] + 1);
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        if (dp[coins.length][amount]!=max){
+            int num=dp[coins.length][amount];
+            return num;
+        }else{
+            return -1;
+        }
+    }
+
+    public int solve4(int[] coins,int amount){
+        int[]dp=new int[amount+1];
+        for (int i = 1; i <=amount ; i++) {
+            dp[i]=max;
+        }
+        for (int i = 0; i <coins.length ; i++) {
+            for (int j = coins[i]; j <=amount ; j++) {
+                dp[j]=Math.min(dp[j],dp[j-coins[i]]+1);
+            }
+        }
+        return dp[coins.length-1]==max?-1:dp[coins.length-1];
+    }
+    /**
+     * 动态规划实现 完全背包
+     * 遍历出所有可能的结果 取最小的结果
+     *
+     * @param coins
+     * @param amount
+     */
     public int coinChange(int[] coins, int amount) {
         /**
          * 递归方式
          */
-        solve1(coins, amount);
+        //solve1(coins, amount);
+        /**
+         * 递归方式优化
+         */
+        //solve2(coins, amount);
         /**
          * 动态规划方式
          */
-        return solve2(coins, amount);
+       return solve3(coins,amount);
+        /**
+         * 动态规划优化
+         */
+        //return solve4(coins,amount);
+    }
+
+    public static void main(String[] args) {
+        CoinChange322 test=new CoinChange322();
+        int[]coins={1,2,5};
+        test.coinChange(coins,11);
     }
 }
